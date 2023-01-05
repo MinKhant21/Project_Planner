9902067a024c18b66fd41ff8d60fd0c5e473371f
minkhant21 <kaungminkhant6889@gmail.com>
2023-01-05 22:25:10 +0630

    u

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,32 @@
 <template>
   <h1>Home</h1>
+  <div v-for="project in projects" :key="project.id">
+    <SigleProject :project="project"></SigleProject>
+  </div>
 </template>
 
 <script>
+import SigleProject from './SigleProject'
 export default {
-    name:'Home'
+  components: { SigleProject },
+    name:'Home',
+    data(){
+      return{
+        projects : [],
+      }
+    },
+    mounted(){
+      fetch("http://localhost:3000/projects")
+      .then((response)=>{
+        return response.json();
+      })
+      .then((data)=>{
+        this.projects = data
+      })
+      .catch(()=>{
+
+      })
+    }
 }
 </script>
 
---
+++ src/components/SigleProject.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="project" @click="ShowDetail=!ShowDetail">
+        <h3>{{ project.title }}</h3>
+        <p v-if="ShowDetail"> {{ project.detail }}</p>
+   </div>
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            ShowDetail:false
+        }
+    }
+}
+</script>
+
+<style>
+    .project{
+        background-color: #f2f2f2;
+        padding: 10px;
+        margin: 10px;
+    }
+    h3{
+        color:indigo;
+    }
+</style>
