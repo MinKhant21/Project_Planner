d508db387d1d9c18a312a222f66d416b1eda9889
minkhant21 <kaungminkhant6889@gmail.com>
2023-01-06 09:14:28 +0630

    u

--- src/components/SigleProject.vue
+++ src/components/SigleProject.vue
@@ -1,7 +1,24 @@
 <template>
-   <div class="project" @click="ShowDetail=!ShowDetail">
-        <h3>{{ project.title }}</h3>
-        <p v-if="ShowDetail"> {{ project.detail }}</p>
+   <div class="project" >
+        <div class="flexing">
+            <div>
+                <h3 @click="ShowDetail=!ShowDetail">{{ project.title }}</h3>
+                <p v-if="ShowDetail"> {{ project.detail }}</p>
+            </div>
+            <div>
+                <span class="material-symbols-outlined">
+                delete
+                </span>
+                <span class="material-symbols-outlined">
+                edit
+                </span>
+                <span class="material-symbols-outlined">
+                done
+                </span>
+            </div>
+        </div>
+        
+        
    </div>
 </template>
 
@@ -21,8 +38,22 @@ export default {
         background-color: #f2f2f2;
         padding: 10px;
         margin: 10px;
+        
+
+    }
+    .flexing{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
     }
     h3{
         color:indigo;
+        cursor: pointer;
+    }
+    span{
+        cursor: pointer;
+    }
+    span:hover{
+        background-color: rgb(188, 148, 156);
     }
 </style>
